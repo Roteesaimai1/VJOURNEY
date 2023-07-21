e6d68781332ffe2dd9216f4fca6c4c22ffe93df2
Roteesaimai <107842127+Roteesaimai1@users.noreply.github.com>
2023-07-21 22:40:38 +0700

    set responsive beta

--- myjourney/src/views/HomeView.vue
+++ myjourney/src/views/HomeView.vue
@@ -2,47 +2,48 @@
   <div class="">
     <!-- Title Text -->
     <div class="text-5xl font-extrabold ">
-      <span class="brightness-125 antialiased animate-pulse pl-5  bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500 hover:bg-clip-text hover:from-violet-500 hover:to-pink-500">
+      <span class="brightness-125 antialiased animate-pulse pl-5 xl:pl-24 bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500 hover:bg-clip-text hover:from-violet-500 hover:to-pink-500">
         V JOURNEY
       </span>
-      <p class="text-base pl-6 items-center">ทัวร์พัทยาฉบับคนท้องถิ่น ฟิน มั่ว รั่วกับชาวประมง</p>
+      <p class="text-base pl-6 xl:pl-24  items-center">ทัวร์พัทยาฉบับคนท้องถิ่น ฟิน มั่ว รั่วกับชาวประมง</p>
       <!-- Card content -->
-      <div class=" max-w-xl bg-white border m-5 border-gray-200 rounded-lg shadow bg-gradient-to-r from-pink-500 to-violet-400 dark:border-gray-700">
-          <a href="#">
+      <div class="xl:grid xl:grid-cols-2 lg:grid lg:grid-cols-2 md:grid md:grid-cols-2">
+      <div class=" max-w-xl bg-white border m-5 border-gray-200 rounded-lg shadow bg-gradient-to-r from-pink-500 to-violet-400 dark:border-gray-700 sm:max-w-5xl lg:max-w-7xl xl:max-w-full xl:m-20">
+          <a href="/trip">
               <img class="rounded-t-lg" src="../assets/fishing.JPG" alt="" />
           </a>
           <div class="p-5">
-              <a href="#">
+              <a href="/trip">
                   <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">กิจกรรมมากมาย</h5>
               </a>
               <p class="mb-3 font-normal text-base text-gray-700 dark:text-white">ดำน้ำ ยิงปลา ตกหมึก กินปลาสด วิ่งหนีตำรวจ และอื่นๆอีกเพียบ</p>             
           </div>
       </div>
       <!-- Card content -->
-      <div class=" max-w-xl bg-white border m-5 border-gray-200 rounded-lg shadow bg-gradient-to-r from-pink-500 to-violet-400 dark:border-gray-700">
-          <a href="#">
+      <div class=" max-w-xl bg-white border m-5 border-gray-200 rounded-lg shadow bg-gradient-to-r from-pink-500 to-violet-400 dark:border-gray-700 sm:max-w-5xl lg:max-w-7xl xl:max-w-full xl:m-20">
+          <a href="/trip">
               <img class="rounded-t-lg" src="../assets/demo-2.JPG" alt="" />
           </a>
           <div class="p-5">
-              <a href="#">
+              <a href="/trip">
                   <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">สิ่งอำนวยความสะดวกครบ</h5>
               </a>
               <p class="mb-3 font-normal text-base text-gray-700 dark:text-white">โรงแรมลับๆดีๆ เก้าอี้แคมป์ปิ้งนั่งริมหาด รถส่วนตัว พัดลม พาวเวอร์แบงค์</p>             
           </div>
       </div>
       <!-- Card content -->
-      <div class=" max-w-xl bg-white border m-5 border-gray-200 rounded-lg shadow bg-gradient-to-r from-pink-500 to-violet-400 dark:border-gray-700">
-          <a href="#">
+      <div class=" max-w-xl bg-white border m-5 border-gray-200 rounded-lg shadow bg-gradient-to-r from-pink-500 to-violet-400 dark:border-gray-700 sm:max-w-5xl lg:max-w-7xl xl:max-w-full xl:m-20">
+          <a href="/trip">
               <img class="rounded-t-lg" src="../assets/seafood.JPG" alt="" />
           </a>
           <div class="p-5">
-              <a href="#">
+              <a href="/trip">
                   <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">อาหารทะเลสดๆ สดกว่านี้ก็ต้องกินข้างเรือ</h5>
               </a>
               <p class="mb-3 font-normal text-base text-gray-700 dark:text-white">ล่าเอง กินเอง หัวใจของความอร่อย ล่าเองไม่ได้ ก็ล่าด้วยแบงค์แดงในตลาดประมงพื้นบ้าน</p>             
           </div>
       </div>
-
+    </div>
     </div>
     
   </div>
--- myjourney/src/views/TripView.vue
+++ myjourney/src/views/TripView.vue
@@ -5,15 +5,15 @@
       <span class="brightness-125 antialiased animate-pulse pl-5 bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
         V TRIP
       </span>
-      <p class="text-base pl-6 items-center">รายการความสนุกของเรามีดังนี้</p>
-      <div class="m-5 max-w-xl grid grid-cols-1 gap-2 h-96 text-center font-medium">
+      <p class="text-base pl-6  items-center">รายการความสนุกของเรามีดังนี้</p>
+      <div class="m-5 max-w-xl grid grid-cols-1 gap-2 h-96 text-center font-medium sm:max-w-5xl lg:max-w-7xl xl:max-w-full xl:grid-cols-2">
         <div class="col-start-1  bg-fuchsia-200   rounded-bl-2xl rounded-tr-2xl text-pink-500 shadow-lg shadow-pink-500/50 text-3xl"><div class="rounded-tr-2xl bg-gradient-to-r from-pink-500 to-violet-400 text-white">SQUID@JOMTIEN BEACH</div>  <hr><span class="text-base pl-2">จุดลับๆสำหรับตกหมึกสดๆยามค่ำคืน บรรยากาศส่วนตัวริมหาดจอมเทียน บางทริปก็กินหมึกเล็ก บางทริปก็หมึกใหญ่ บางทริปก็กินน้ำฝน</span> 
         <p class="text-xs animate-ping text-red-500 m-2"><router-link to="/contact">ทัวร์เลย</router-link></p></div>
-        <div class="col-start-1  bg-fuchsia-200   rounded-bl-2xl rounded-tr-2xl text-pink-500 shadow-lg shadow-pink-500/50 text-3xl"><div class="rounded-tr-2xl bg-gradient-to-r from-pink-500  to-violet-400 text-white">FISHING SUNLIGHT</div>  <hr><span class="text-base pl-2">ตกปลาเก๋าล่าวัตถุดิบยามเย็น วิวสวยเห็นพระอาทิตย์ตก ถ้าได้ปลาก็กินอาหารสด ไม่ได้ก็อาหารกระป๋อง</span> 
+        <div class="col-start-1 xl:col-start-2 bg-fuchsia-200   rounded-bl-2xl rounded-tr-2xl text-pink-500 shadow-lg shadow-pink-500/50 text-3xl"><div class="rounded-tr-2xl bg-gradient-to-r from-pink-500  to-violet-400 text-white">FISHING SUNLIGHT</div>  <hr><span class="text-base pl-2">ตกปลาเก๋าล่าวัตถุดิบยามเย็น วิวสวยเห็นพระอาทิตย์ตก ถ้าได้ปลาก็กินอาหารสด ไม่ได้ก็อาหารกระป๋อง</span> 
         <p class="text-xs animate-ping text-red-500 m-2"><router-link to="/contact">ทัวร์เลย</router-link></p></div>
         <div class="col-start-1  bg-fuchsia-200  rounded-bl-2xl rounded-tr-2xl text-pink-500 shadow-lg shadow-pink-500/50 text-3xl"><div class="rounded-tr-2xl bg-gradient-to-r from-pink-500  to-violet-400 text-white">NIGHT PARTY</div>  <hr><span class="text-base pl-2">ผับเด็ดๆดังๆ ในพัทยาฉบับเข้าทางออก ออกทางเข้า ถ้าภาษาดี มีกินฟรีตลอดงาน</span> 
         <p class="text-xs animate-ping text-red-500 m-2"><router-link to="/contact">ทัวร์เลย</router-link></p></div>
-        <div class="col-start-1  bg-fuchsia-200  rounded-bl-2xl rounded-tr-2xl text-pink-500 shadow-lg shadow-pink-500/50 text-3xl"><div class="rounded-tr-2xl bg-gradient-to-r from-pink-500  to-violet-400 text-white">NIGHT MARKET</div>  <hr><span class="text-base pl-2">ยกขโยงกันมาทั้งพัทยา ร้านเด็ดร้านดัง เหล้า ยา ปลาปิ้ง ครบจบในตลาดเดียว</span> 
+        <div class="col-start-1 xl:col-start-2 bg-fuchsia-200  rounded-bl-2xl rounded-tr-2xl text-pink-500 shadow-lg shadow-pink-500/50 text-3xl"><div class="rounded-tr-2xl bg-gradient-to-r from-pink-500  to-violet-400 text-white">NIGHT MARKET</div>  <hr><span class="text-base pl-2">ยกขโยงกันมาทั้งพัทยา ร้านเด็ดร้านดัง เหล้า ยา ปลาปิ้ง ครบจบในตลาดเดียว</span> 
         <p class="text-xs animate-ping text-red-500 m-2"><router-link to="/contact">ทัวร์เลย</router-link></p></div>
       </div>
     </div>   
