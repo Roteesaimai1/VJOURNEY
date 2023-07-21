ff0571749b1d6cd4ef4d153364ff2ad86ea28a64
Roteesaimai <107842127+Roteesaimai1@users.noreply.github.com>
2023-07-21 12:06:39 +0700

    navbar scroll to bg transparent

--- myjourney/src/components/navbar.vue
+++ myjourney/src/components/navbar.vue
@@ -1,16 +1,40 @@
 <template>
-    <div class="grid gap-4 grid-cols-3 bg-zinc-950 rounded-r-2xl  border-b-2 border-pink-600 shadow-lg shadow-pink-500/20 brightness-125">
-        <router-link to="/" class="antialiased  text-3xl m-3 font-semibold bg-clip-text text-transparent bg-gradient-to-r from-rose-500 hover:from-rose-500 hover:to-fuchsia-500 ">Home</router-link>
-        <router-link to="/trip" class="antialiased text-3xl m-3 font-semibold bg-clip-text text-transparent bg-gradient-to-r from-rose-500 hover:from-rose-500 hover:to-fuchsia-500 ">Trip</router-link>
-        <router-link to="/about" class="antialiased text-3xl m-3 font-semibold bg-clip-text text-transparent bg-gradient-to-r from-rose-500 hover:from-rose-500 hover:to-fuchsia-500 ">About</router-link>
+    <div class="relative">
+        <div :style="navbarStyle" class="fixed top-0 left-0 right-0 grid gap-4 grid-cols-3 rounded-r-2xl  border-b-2 border-pink-600 shadow-lg shadow-pink-500/20 brightness-125">
+            <router-link to="/" class="antialiased  text-3xl m-3 font-semibold bg-clip-text text-transparent bg-gradient-to-r from-rose-500 hover:from-rose-500 hover:to-fuchsia-500 ">Home</router-link>
+            <router-link to="/trip" class="antialiased text-3xl m-3 font-semibold bg-clip-text text-transparent bg-gradient-to-r from-rose-500 hover:from-rose-500 hover:to-fuchsia-500 ">Trip</router-link>
+            <router-link to="/about" class="antialiased text-3xl m-3 font-semibold bg-clip-text text-transparent bg-gradient-to-r from-rose-500 hover:from-rose-500 hover:to-fuchsia-500 ">About</router-link>
+        </div>
     </div>
+    
 </template>
 
 <script>
     export default {
-        name:'navbar',        
+        name:'navbar',     
+        data() {
+            return {
+                navbarStyle: {
+                    background: 'rgba(9, 9, 11, 0.8)', //bg navbar starter
+                }
+            }
+        },
+        mounted(){
+            window.addEventListener('scroll', this.updateNavbarStyle)
+        },
+        beforeDestroy() {
+            window.removeEventListener('scroll', this.updateNavbarStyle);
+        },
+        methods: {
+            updateNavbarStyle() {
+                const scrollPercent = (100 * window.scrollY) / (document.documentElement.scrollHeight - window.innerHeight);
+                const navbarOpacity = 1 - (scrollPercent / 100);
+                this.navbarStyle.background = `rgba((9, 9, 11, ${navbarOpacity})`;
+            },    
+        }   
     }
 </script>
+
 
 <style scoped>
 
